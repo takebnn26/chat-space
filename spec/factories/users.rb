395f6c19fd3bda6cd92556126e3a2894bd218cfd
hayato takemoto <takebnn26@gmail.com>
2016-10-29 16:04:50 +0900

    testing user model

---
+++ spec/factories/users.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+
+  factory :user do
+    name                    "hayato"
+    email                   "hayato@haya.com"
+    password                "hahahaha"
+    password_confirmation   "hahahaha"
+  end
+
+end
