9348ad1c1d81469da0a5f452e922bb5827dcd2d6
hayato takemoto <takebnn26@gmail.com>
2016-10-29 13:18:30 +0900

    setting routing for users/edit,update and users_controller
    setting application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,27 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :configure_permitted_parameters, if: :devise_controller?
-
   before_action :authenticate_user!
+
+  class PermissionDenied < StandardError; end
+
+  rescue_from PermissionDenied do
+    redirect_to :root, alert: "ユーザーであるか確認してください。"
+  end
 
   private
 
+  def after_sign_out_path_for(resource)
+    new_user_session_path
+  end
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
+
+  def comfirm_user(resource)
+    raise PermissionDenied if resource != current_user
   end
 
 end
