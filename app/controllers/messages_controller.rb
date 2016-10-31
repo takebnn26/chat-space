60beb747d557385632d45ee4df2d99b4b1a1cafc
hayato takemoto <takebnn26@gmail.com>
2016-10-31 18:21:02 +0900

    fixing

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,7 @@
 class MessagesController < ApplicationController
 
   def index
-    @groups = current_user.groups.all
+    @groups = current_user.groups
   end
 
   def create
--- app/models/group.rb
+++ app/models/group.rb
@@ -2,10 +2,8 @@ class Group < ApplicationRecord
 
   has_many :users,       through: :group_users
   has_many :messages,    dependent: :destroy
-  has_many :group_users
+  has_many :group_users, dependent: :destroy
 
   validates :name, presence: true
-
-  accepts_nested_attributes_for :users
 
 end
