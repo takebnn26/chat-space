aab8b73f18df70948a38d1a19ab66757da170576
hayato takemoto <takebnn26@gmail.com>
2016-11-17 20:31:16 +0900

    fixing routes for resources messages with nest
    fixing view files

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  root 'messages#index'
-  resources :messages, only: [:index, :create]
+  root 'groups#index'
+  resources :groups, except: [:index, :delete] do
+    resources :messages, only: [:index, :create]
+  end
   resources :users,    only: [:edit, :update]
-  resources :groups,   except: [:index, :delete]
 
 end
