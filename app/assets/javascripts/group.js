26d8c0e5176fd69c3470c01d106b91ea5c13fabe
hayato takemoto <takebnn26@gmail.com>
2016-11-25 15:32:13 +0900

    create incremental search
    HTML挿入のフォーマット作成
    ajax通信の修正　HTML挿入　url修正
    クリックイベント作成

--- app/assets/javascripts/group.js
+++ app/assets/javascripts/group.js
@@ -1,49 +1,90 @@
 $(document).on('turbolinks:load', function(){
 
-  function insertUsersList(user) {
+//インクリメンタルサーチ時のユーザリストのHTML
+  function insertUsersList(id, name) {
     var list = $(
-                "<div id='user-search-result'>" +
-                  "<div class='chat-group-user users_list clearfix'>" +
-                    "<p class='chat-group-users__name'>" +
-                      user.name +
-                    "</p>" +
-                  "</div>" +
+                "<div class='chat-group-user users_list clearfix'>" +
+                  "<p class='chat-group-users__name'>" +
+                    name +
+                    "<a class='user-search-add add_users chat-group-user__btn chat-group-user__btn--add' data-user-id=" + id + " data-user-name=" + name + ">追加" +
+                    "</a>" +
+                  "</p>" +
                 "</div>"
                 );
-    $('.chat-group-form__search').append(list);
+    return list
+  };
+
+//追加ボタンを押した時のユーザーリストのHTML
+  function appendUser(id, name) {
+    var appendUserHtml = $(
+                          "<div class='chat-group-user remove_list clearfix'>" +
+                            "<input type='hidden' name='group[user_ids][]' value=" + id + ">" +
+                            "<p class='chat-group-users__name'>" + name +
+                              "<a class='user-search-remove remove_users chat-group-user__btn chat-group-user__btn--remove'>削除" +
+                              "</a>" +
+                            "</p>" +
+                          "</div>"
+                          );
+    return appendUserHtml
   };
 
 
-  var preFunc = null;
-  var preInput = '';
-  var input = '';
 
-
+//ajax通信
   function ajaxSearch(input) {
+
+    var result = $('#user-search-result');
+
     $.ajax({
-      url:      'search',
+      url:      '/groups/search',
       type:     'GET',
       dataType: 'json',
       data:     ( "keyword=" + input )
     })
+
     .done(function(data) {
       $('.users_list').remove();
       $.each(data, function(i, user) {
-        insertUsersList(user);
+        $(result).append(insertUsersList(user.id, user.name));
       });
+      $(result).appendTo($('.chat-group-form__search'));
     })
+
     .fail(function(data) {
       alert("エラーが発生しました")
     });
   };
 
 
+//削除ボタンを押した時のイベント
+  $(document).on("click", ".remove_users", function() {
+    var $this = $(this);
+    $this.parents(".remove_list").remove();
+  });
+
+
+//追加ボタンを押した時のイベント
+  $(document).on("click", ".add_users", function() {
+    var $this = $(this);
+    var id = $this.data("userId");
+    var name = $this.data("userName");
+    $this.parents(".users_list").remove();
+    $("#chat-group-users").append(appendUser(id, name));
+  });
+
+
+//インクリメンタルサーチ検索欄にキー入力したときのイベント
+  var preFunc = null;
+  var preInput = '';
+  var input = '';
+
   $('#user-search-field').on("keyup", function(e) {
+
     var input = $.trim($(this).val());
     if(preInput !== input) {
       clearTimeout(preFunc);
       preFunc = setTimeout(ajaxSearch(input), 1000);
     }
-    preinput = input;
+    preInput = input;
   });
 });
